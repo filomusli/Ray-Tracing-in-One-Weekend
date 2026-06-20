ab78113d2f8f92c8aa8d29e3c3bab48d7111c058
filomusli <216780010+filomusli@users.noreply.github.com>
2026-06-20 14:26:07 +0200

    Add project files.

---
+++ Ray Tracing in One Weekend/Ray Tracing in One Weekend.cpp
@@ -0,0 +1,12 @@
+﻿// Ray Tracing in One Weekend.cpp : Defines the entry point for the application.
+//
+
+#include "Ray Tracing in One Weekend.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
