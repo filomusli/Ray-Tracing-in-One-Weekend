b5097f16527d34a2cd5e3ca29454a52731271453
filomusli <216780010+filomusli@users.noreply.github.com>
2026-06-20 21:36:50 +0200

    added functions

--- Ray Tracing in One Weekend/vec3.h
+++ Ray Tracing in One Weekend/vec3.h
@@ -62,5 +62,49 @@ public:
 	}
 };
 
+// You can write point3 and it will give you a vec3. 
+// Apparently it's called an alias
+using point3 = vec3;
+
+// Vector utility functions
+
+inline std::ostream& operator<<(std::ostream& out, const vec3& v) {
+	return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
+}
+
+inline vec3 operator+(const vec3& u, const vec3& v) {
+	return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
+}
+
+inline vec3 operator*(const vec3& u, const vec3& v) {
+	return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
+}
+
+inline vec3 operator*(double t, const vec3& v) {
+	return vec3(t * v.e[0], t * v.e[1], t * v.e[2]);
+}
+
+inline vec3 operator*(const vec3& v, double t) {
+	return t * v;
+}
+
+inline vec3 operator/(const vec3& v, double t) {
+	return v * (1/t);
+}
+
+inline double dot(const vec3& u, const vec3& v) {
+	return u.e[0] * v.e[0] + u.e[1] * v.e[1] + u.e[2] * v.e[2];
+}
+
+inline vec3 cross(const vec3& u, const vec3& v) {
+	return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
+				u.e[2] * v.e[0] - u.e[0] * v.e[2],
+				u.e[0] * v.e[1] - u.e[1] * v.e[0]);
+}
+
+inline vec3 unit_vector(const vec3& v) {
+	return v / v.length();
+}
+
 
 #endif
